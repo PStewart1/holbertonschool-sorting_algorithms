cd7a95ee67213fb96e46fd24a40034a459a1ea2c
PStewart1 <landofalcon007@gmail.com>
2022-08-16 16:36:05 -0500

    added extra guard for null prev edge case

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *node, *temp, *head;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (!*list || (!(*list)->next && !(*list)->prev))
 		return;
 
 	head = (*list)->next;
