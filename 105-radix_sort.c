233e37ae5ef928b3915d8d61610564ff39628526
PStewart1 <4920@holbertonstudents.com>
2023-02-18 18:42:06 -0600

    sorts an array of ints using the LSD Radix sort algorithm

---
+++ 105-radix_sort.c
@@ -0,0 +1,92 @@
+#include "sort.h"
+
+/**
+ * getMax - Function to get the largest element from an array
+ * @array: array
+ * @size: size of array
+ * Return: max element
+ */
+int getMax(int array[], int size)
+{
+	int max = array[0];
+
+	for (int i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	return (max);
+}
+
+/**
+ * countingSort - sorting to buckets in matrix, by significant places
+ * @array: array to sort
+ * @size: size of array
+ * @place: current place we're sorting by
+ * @o: number of 0's in array
+ * Return: void
+ */
+void sorting_out(int array[], int size, int place, int o)
+{
+	int(*matrix)[10] = malloc(size * sizeof(*matrix));
+	int i, j, k, n;
+
+	for (i = 0; i < size; i++)
+	{
+		n = (array[i] / place) % 10;
+		for (j = 0; j < 10; j++)
+		{
+			if (matrix[n][j] == 0)
+			{
+				matrix[n][j] = array[i];
+				break;
+			}
+		}
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (o > 0)
+		{
+			array[i] = 0;
+			o--;
+			continue;
+		}
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				if (matrix[j][k] != 0)
+				{
+					array[i] = matrix[j][k];
+					i++;
+					if (i == size)
+						return;
+				}
+				else
+					break;
+			}
+		}
+	}
+	free(matrix);
+}
+
+/**
+ * radix_sort - sorts an array of ints using the LSD Radix sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ * Return: void
+ */
+void radix_sort(int *array, size_t size)
+{
+	int place, max, i, o = 0;
+
+	max = getMax(array, size);
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] == 0)
+			o++;
+	}
+	for (place = 1; max / place > 0; place *= 10)
+	{
+		sorting_out(array, size, place, o);
+		print_array(array, size);
+	}
+}
